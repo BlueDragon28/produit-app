878c13dfde98aeb9dd728a9a1231c26558852a72
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-07-17 11:20:04 +0200

    ajout du model Produit

--- gestion_produit/models.py
+++ gestion_produit/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+from django.db.models import Model
+from django.core.validators import MinLengthValidator, MinValueValidator
+
+class Produit(Model):
+    name = models.CharField(max_length=300, null=False, unique=True, validators=[MinLengthValidator(3, "Le nom du produit doit contenir au moins 3 charactères")])
+    prix_unitaire = models.DecimalField(null=False, max_digits=4, decimal_places=2)
+    quantite = models.IntegerField(null=False, validators=[MinValueValidator(0, "La  quantité de produit doit être égale ou supérieur à 0")])
