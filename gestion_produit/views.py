243dbeb02e5fea2e8283174ac66004a6e7856c55
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-07-17 14:11:04 +0200

    ajout SearchFilter backend pour rechercher dans la liste de produits

--- gestion_produit/views.py
+++ gestion_produit/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
 
 from .models import Produit
 from .serializers import ProduitSerializer, HREFProduitSerializer
@@ -6,6 +7,8 @@ from .serializers import ProduitSerializer, HREFProduitSerializer
 class ProduitViewSet(ModelViewSet):
     serializer_class = HREFProduitSerializer
     queryset = Produit.objects.all()
+    filter_backends = [SearchFilter]
+    search_fields = ["name"]
 
     def create(self, request, *args, **kwargs):
         self.serializer_class = ProduitSerializer
