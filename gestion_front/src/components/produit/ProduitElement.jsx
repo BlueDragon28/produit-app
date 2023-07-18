2e23f190483e083c382394c4dd7df90744b66a9a
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-07-18 08:17:28 +0200

    ajout effet survol aux produits

--- gestion_front/src/components/produit/ProduitElement.jsx
+++ gestion_front/src/components/produit/ProduitElement.jsx
@@ -1,8 +1,10 @@
+import styles from "./ProduitElement.module.css";
+
 function ProduitElement({ produit }) {
     const { name, prix_unitaire } = produit;
 
     return (
-        <div>
+        <div class={styles["produit-element"]}>
             {name} - {prix_unitaire}
         </div>
     );
